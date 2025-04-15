2aa1c68c69a27146fce0b621ffb4cce94acafc7d
Your Full Name <your-email@example.com>
2025-04-15 16:50:59 -0600

    Fix: match CodeGrade variable setup

--- gardenManagement.js
+++ gardenManagement.js
@@ -2,23 +2,23 @@ const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
 
-// Check temperature
 if (temperature > 80) {
-    console.log("Watering on");
-  } else {
-    console.log("Watering off");
-  }
-  
-  // Check time of day
-  if (timeOfDay === "evening" || timeOfDay === "night") {
-    console.log("Lights on");
-  } else {
-    console.log("Lights off");
-  }
-  
-  // Increase soil moisture to 40
-  while (soilMoisture < 40) {
-    console.log(soilMoisture);
-    soilMoisture += 5;
-  }
+  console.log("Watering on");
+} else {
+  console.log("Watering off");
+}
+
+if (timeOfDay === "evening" || timeOfDay === "night") {
+  console.log("Lights on");
+} else {
+  console.log("Lights off");
+}
+
+while (soilMoisture < 40) {
   console.log(soilMoisture);
+  soilMoisture += 5;
+}
+console.log(soilMoisture);
+
+
+
